b40e06fda1418035a5aa48961bb7fab3851518c4
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-28 18:51:37 +0300

    Taks 10 updated again

--- task10/Program.cs
+++ task10/Program.cs
@@ -4,5 +4,13 @@
         return temp%100/10;
 }
 Console.Write("Введите трехзначное число: ");
+
 int a = int.Parse(Console.ReadLine());
-Console.WriteLine(SecondDigitFrom3(a));
+if (a < 100 || a > 999)
+{
+        Console.WriteLine("Вы ввели число неправильно");        
+}
+else
+{
+        Console.WriteLine(SecondDigitFrom3(a));
+}
